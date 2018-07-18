2e5d7a4a23dcacb17c315b71b2a2a5cde87b6563
Abinav Ravi Venkatakrishnan <31827335+AbinavRavi@users.noreply.github.com>
2018-07-18 22:43:37 +0200

    Changes

--- Assignment3/ex3.cpp
+++ Assignment3/ex3.cpp
@@ -4,52 +4,40 @@
 using namespace std;
 namespace Manta {
 
-	KERNEL(bnd = 1)
+	KERNEL(bnd = 1,single)
 	void ComputeDiv(FlagGrid& flags, Grid<Real>& div, MACGrid& vel){
-		// ...
-		if (flags.isFluid)
-		{
-			div(i,j,k) = vel(i+0.5,j,k)-vel(i-0.5,j,k) + vel(i,j+0.5,k) - vel(i,j-0.5,k);
-		}
+	//compute divergence
+		if (flags.isFluid(i, j,k)) 
+			div(i,j,k) = (vel(i+1,j,k).x-vel(i,j,k).x + vel(i,j+1,k).y - vel(i,j,k).y);
 	}
 
-	KERNEL(bnd = 1)
+	KERNEL(bnd = 1,single)
 	void iterate_p(const Grid<Real>& div, Grid<Real>& pressure, const Grid<Real> &A0, const Grid<Real> &A1) {
-		// ...
-		//pressure equation is of the form Ap = d
-		pressure(i,j,k) = 1*0.25/A1(i,j,k)*(div(i,j,k)-A1(i+1,j,k)*pressure(i+1,j,k)-A1(i,j+1,k)*pressure(i,j+1,k)-A0(i-1,j,k)*pressure(i-1,j,k)-A0(i,j-1,k)*pressure(i,j-1,k));
-
+		pressure(i,j,k) = 1/A1(i,j,k)*(div(i,j,k)-A0(i+1,j,k)*pressure(i+1,j,k)-A0(i,j+1,k)*pressure(i,j+1,k)-A0(i-1,j,k)*pressure(i-1,j,k)-A0(i,j-1,k)*pressure(i,j-1,k));
 	}
 
-	KERNEL(bnd = 1, reduce=+) returns(double residual=0.0)
-	double iterate_r(const Grid<Real>& div, Grid<Real>& pressure, const Grid<Real> &A0, const Grid<Real> &A1) {
-		// ...
-		//residual is given by r = d- Ap(new)
-		residual(i,j,k) = div(i,j,k) - -A1(i+1,j,k)*pressure(i+1,j,k)-A1(i,j+1,k)*pressure(i,j+1,k)-A0(i-1,j,k)*pressure(i-1,j,k)-A0(i,j-1,k)*pressure(i,j-1,k);
-
+	KERNEL(bnd = 1, reduce=+)returns(Real res=0.0)
+	Real iterate_r(const Grid<Real>& div, Grid<Real>& pressure, Grid<Real>& residual, const Grid<Real> &A0, const Grid<Real> &A1) {
+	//r = d - A*pnew
+		res = div(i,j,k) - A1(i,j,k)*pressure(i,j,k) - A0(i-1,j,k)*pressure(i-1,j,k) - A0(i,j-1,k)*pressure(i,j-1,k) - A0(i+1,j,k)*pressure(i+1,j,k) - A0(i,j+1,k)*pressure(i,j+1,k);
+		residual(i,j,k) = res;
 	}
 
-	KERNEL(bnd = 1, reduce=+) returns(double residual=0.0)
-	double iterate2(const Grid<Real>& div, Grid<Real>& pressure, const Grid<Real> &A0, const Grid<Real> &A1) {
-		// ...
-		residual(i,j,k) = div(i,j,k) - -A1(i+1,j,k)*pressure(i+1,j,k)-A1(i,j+1,k)*pressure(i,j+1,k)-A0(i-1,j,k)*pressure(i-1,j,k)-A0(i,j-1,k)*pressure(i,j-1,k);
-		pressure(i,j,k) = residual(i,j,k)/A1(i,j,k);
-
-		
+	KERNEL(bnd = 1, reduce=+)returns(Real res=0.0)
+	Real iterate2(const Grid<Real>& div, Grid<Real>& pressure, Grid<Real>& residual, const Grid<Real> &A0, const Grid<Real> &A1) {
+		res = div(i,j,k) - A1(i,j,k)*pressure(i,j,k) - A0(i-1,j,k)*pressure(i-1,j,k) - A0(i,j-1,k)*pressure(i,j-1,k) - A0(i+1,j,k)*pressure(i+1,j,k) - A0(i,j+1,k)*pressure(i,j+1,k);
+		residual(i,j,k) = res;
+		pressure(i,j,k) = (residual(i,j,k))/A1(i,j,k) + pressure(i,j,k);
 	}
 
 	KERNEL(bnd = 1)
 	void UpdateVel(const FlagGrid& flags, MACGrid& vel, const Grid<Real>& pressure){
-		// ...
-		if (flags.isFluid)
-		{
-			vel(i,j,k) = vel(i,j,k) - Vec3(pressure(i-1,j,k)-pressure(i,j,k), pressure(i,j-1,k)-pressure(i,j,k)); 
-		}
+		if (flags.isFluid(i, j,k))
+			vel(i,j,k) = vel(i,j,k) - Vec3(pressure(i-1,j,k)-pressure(i,j,k), pressure(i,j-1,k)-pressure(i,j,k), vel(i,j,k).z); 
 	}
 
-	KERNEL(idx)
+	KERNEL()
 	void ComputeA0(const FlagGrid& flags, Grid<Real> &A0) {
-		// ...
 		if(flags.isFluid(i, j,k))
 			A0(i,j,k) = -1;
 		else 
@@ -58,12 +46,10 @@ namespace Manta {
 
 	KERNEL(bnd = 1)
 	void ComputeA1(const Grid<Real> &A0, Grid<Real> &A1) {
-		// ...
 		A1(i,j,k) = 0.0-(A0(i-1,j,k) + A0(i,j-1,k) + A0(i+1,j,k) + A0(i,j+1,k));
 	}
 
 	PYTHON() void solvePressureGS(FlagGrid& flags, MACGrid& vel, Grid<Real>& pressure, Real gsAccuracy = 1e-4) {
-		// ...
 		FluidSolver* parent = flags.getParent();
 		Grid<Real> div(parent);
 		Real res = 1.0;
@@ -79,17 +65,15 @@ namespace Manta {
 		{
 			iterate_p(div, pressure, A0, A1);
 			res = iterate_r(div, pressure, residual, A0, A1);
-			//res = iterate2(div, pressure, residual, A0, A1);
 		}
 		UpdateVel(flags, vel, pressure);
+
 	}
 
 	PYTHON() Real getMaxDivergence(MACGrid& vel, FlagGrid& flags){
-		// ...
 		FluidSolver* parent = flags.getParent();
 		Grid<Real> div(parent);
-		return totalDivr(flags, div, vel)/totalNumber(div);
-		return 0.;
+		//return totalDivr(flags, div, vel)/totalNumber(div);
 	}
 
 } // end namespace
